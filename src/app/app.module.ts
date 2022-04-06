964b007d3318619158a97087563745345031d867
Huy Nguyen Gia <intern.ngiahuy1@tma.com.vn>
2022-04-06 17:48:41 +0700

    creat form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogningPageComponent } from './components/logning-page/logning-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogningPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/logning-page/logning-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logning-page',
+  templateUrl: './logning-page.component.html',
+  styleUrls: ['./logning-page.component.css']
+})
+export class LogningPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
